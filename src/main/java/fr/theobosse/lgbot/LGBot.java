cf4a914253224f8a7e6dfa95a00417b39f1986f7
TheodoreBillotte <theodore.billotte@epitech.eu>
2023-02-03 14:13:25 +0100

    Loup-Garou [ADD]: new role -> loup-garou blanc this is lie a classic werewolf, but he has to kill everyone

--- src/main/java/fr/theobosse/lgbot/LGBot.java
+++ src/main/java/fr/theobosse/lgbot/LGBot.java
@@ -105,10 +105,8 @@ public class LGBot {
         Roles.addRole(new Role("Sorcière", "Sorcière", Clan.VILLAGE, Emotes.getEmote("witch"), Rounds.WITCH, new Witch()));
         Roles.addRole(new Role("Corbeau", "Corbeau", Clan.VILLAGE, Emotes.getEmote("crow"), Rounds.CROW, new Crow()));
         Roles.addRole(new Role("Voyante", "Voyante", Clan.VILLAGE, Emotes.getEmote("seer"), Rounds.SEER, new Seer()));
-         Roles.addRole(new Role("Cupidon", "Cupidon", Clan.VILLAGE, Emotes.getEmote("cupid"), Rounds.CUPID, new Cupidon()));
-        // Roles.addRole(new Role("Renard", "Renard", Clan.VILLAGE, Emotes.getEmote("fox"), Rounds.FOX, null));
-        // Roles.addRole(new Role("Loup-Garou Blanc", "LGB", Clan.SOLO, Emotes.getEmote("white_werewolf"), Rounds.WEREWOLF, null));
-        // Roles.addRole(new Role("Assassin", "Assassin", Clan.SOLO, Emotes.getEmote("killer"), Rounds.KILLER, null));
+        Roles.addRole(new Role("Cupidon", "Cupidon", Clan.VILLAGE, Emotes.getEmote("cupid"), Rounds.CUPID, new Cupidon()));
+        Roles.addRole(new Role("Loup-Garou Blanc", "LGB", Clan.SOLO, Emotes.getEmote("white_werewolf"), Rounds.WEREWOLF, new WereWolf()));
     }
 
     public static void loadMembers() {
